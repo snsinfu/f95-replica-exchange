ae1be940e9b9259d84e852e58b994e58c6d15f1b
snsinfu <snsinfu@gmail.com>
2020-09-17 17:52:42 +0900

    Fix langevin sampling

--- main.f95
+++ main.f95
@@ -3,34 +3,39 @@ program main
 
     implicit none
 
-    real :: temps(3) = (/ 0.1, 1.0, 2.0 /)
+    real :: temps(5) = (/ 0.1, 0.3, 0.8, 1.2, 1.7 /)
     integer(8) :: seed
     integer :: rank
     integer :: replica_id
     integer :: step
-    real :: energy
     logical :: ok
 
-    real :: dt = 0.001
+    real :: dt = 0.005
     real :: x = 0
     real :: v = 0
     real :: F = 0
+    real :: R = 0
+    real :: energy
     real :: kT
+    real :: friction
+    real :: damping
+    real :: sigma
 
     seed = 1
+    friction = 0.05
 
     call rex_init(temps)
     call rex_seed(seed)
     call rex_rank(rank)
     call rex_id(replica_id)
 
-    do step = 0, 10000
+    do step = 0, 100000
         ! Potential energy
-        energy = (x + 3.5)**2 + sin(x - 2.5)
+        energy = system_energy(x, v)
         kT = temps(replica_id)
 
-        if (replica_id == 1 .and. mod(step, 100) == 0) then
-            print "(i5,a,f8.3,a,f6.3)", step, " ", x, " ", energy
+        if (replica_id == 1 .and. mod(step, 10) == 0) then
+            print "(i6,a,f8.3,a,f6.3)", step, " ", x, " ", energy
         end if
 
         if (mod(step, 100) == 0) then
@@ -38,13 +43,52 @@ program main
             call rex_id(replica_id)
         end if
 
-        v = v * sqrt(kT) / (abs(v) + 1e-6)
+        damping = exp(-friction * dt)
+        sigma = sqrt(kT * (1 - exp(-2 * friction * dt)))
+        call random_normal(R)
 
-        x = x + dt * v
-        F = -2 * (x + 3.5) - 2 * (x - 2.5)
-        v = v + dt * F
+        v = v + dt * F / 2
+        x = x + dt * v / 2
+        v = v * damping
+        v = v + sigma * R
+        x = x + dt * v / 2
+        F = system_force(x)
+        v = v + dt * F / 2
     end do
 
     call rex_finalize()
 
+    stop
+
+contains
+    real function system_energy(x, v)
+        real, intent(in) :: x
+        real, intent(in) :: v
+
+        system_energy = 0.5 * (x**2 + sin(5 * x)) + v * v / 2
+    end function
+
+    real function system_force(x)
+        real, intent(in) :: x
+
+        system_force = 0.5 * (-2 * x - 5 * cos(5 * x))
+    end function
+
+    subroutine random_normal(z)
+        real, intent(out) :: z
+        real :: u, v
+
+        do
+            call random_number(u)
+            call random_number(v)
+
+            if (u > 1e-6) then
+                exit
+            end if
+        end do
+
+        z = sqrt(-2 * log(u)) * cos(6.2831853 * v)
+
+        return
+    end subroutine
 end program
